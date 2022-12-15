d11daf43bc8078674138260828a2b3cb85066cf4
Gautier LAURENT <gautier.laurent@univ-orleans.fr>
2022-12-15 14:40:56 +0100

    make package installable

---
+++ GeoDataKit/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""GeoDataKit package for analysing and ploting geoscientific data
+
+Several modules are provided:
+    dataset: providing example datasets
+    geoplot: providing visualisation tools
+    utils: for internal helper functions and classes
+"""
