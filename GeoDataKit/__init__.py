8d933e86ddc93cff4e140b3c0c03540896cf0940
Gautier Laurent <gautier.laurent@univ-orleans.fr>
2023-07-11 16:42:22 +0200

    trying to make installation optional for notebooks

--- GeoDataKit/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-"""GeoDataKit package for analysing and ploting geoscientific data
-
-Several modules are provided:
-    dataset: providing example datasets
-    geoplot: providing visualisation tools
-    utils: for internal helper functions and classes
-"""
